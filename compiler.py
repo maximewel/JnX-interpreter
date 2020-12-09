2bd731c563cbd5dfc1893e1a0d6f48e929e751df
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2020-12-09 22:42:34 +0100

    init compiler with .vm compiler ("similar" output as text)

--- compiler.py
+++ compiler.py
@@ -3,52 +3,82 @@ from ast import addToClass
 from functools import reduce
 from parserast import parse
 import sys
+import os
 
-#init ops and vars
-ops = {
-    '+' : lambda x,y : x+y,
-    '-' : lambda x,y : x-y,
-    '*' : lambda x,y : x*y,
-    '/' : lambda x,y : x/y,
-}
-vars = {}
 
-#interpret functions
+#init variables
+
+
+def whilecounter():
+    whilecounter.current += 1
+    return whilecounter.current
+whilecounter.current = 0
+
+#programme
 @addToClass(ast.ProgramNode)
-def execute(self):
+def compile(self):
+    bytecode = ""
     for c in self.children:
-        c.execute()
+        bytecode += c.compile()
+    return bytecode
 
+#node variable
 @addToClass(ast.TokenNode)
-def execute(self):
+def compile(self):
+    bytecode = ""
     if isinstance(self.tok, str):
-        try:
-            return vars[self.tok]
-        except KeyError:
-            print(f"Error : Variable {self.tok} undefined !")
-    return self.tok
-
-@addToClass(ast.OpNode)
-def execute(self):
-    args = [c.execute() for c in self.children]
-    if len(args) == 1:
-        args.insert(0,0)
-    return reduce(ops[self.op], args)
+        bytecode += "PUSHV %s\n" % self.tok
+    else:
+        bytecode += "PUSHC %s\n" % self.tok
+    return bytecode
 
 @addToClass(ast.AssignNode)
-def execute(self):
-    vars[self.children[0].tok] = self.children[1].execute()
-
+def compile(self):
+    bytecode = ""
+    bytecode += self.children[1].compile()
+    bytecode += "SET %s\n" % self.children[0].tok
+    return bytecode
+    
+#print
 @addToClass(ast.PrintNode)
-def execute(self):
-    print(self.children[0].execute())
-
+def compile(self):
+    bytecode = ""
+    bytecode += self.children[0].compile()
+    bytecode += "PRINT\n"
+    return bytecode
+    
+#operation
+@addToClass(ast.OpNode)
+def compile(self):
+    bytecode = ""
+    if len(self.children) == 1:
+        bytecode += self.children[0].compile()
+        bytecode += "USUB\n"
+    else:
+        for c in self.children:
+            bytecode += c.compile()
+        bytecode += ops[self.op] + "\n"
+    return bytecode
+    
+#while
 @addToClass(ast.WhileNode)
-def execute(self):
-    while(self.children[0].execute()):
-        self.children[1].execute()
+def compile(self):
+    counter = whilecounter()
+    bytecode = ""
+    bytecode += "JMP cond%s\n" % counter
+    bytecode += "body%s: " % counter
+    bytecode += self.children[1].compile()
+    bytecode += "cond%s: " % counter
+    bytecode += self.children[0].compile()
+    bytecode += "JINZ body%s\n" % counter
+    return bytecode
 
 if __name__ == "__main__":
     prog = open(sys.argv[1]).read()
     ast = parse(prog)
-    ast.execute()
+    compiled = ast.compile()
+    name = os.path.splitext(sys.argv[1])[0]+'.vm'
+    outfile = open(name,'w')
+    outfile.write(compiled)
+    outfile.close()
+    print("Wrote output to",name)
